edf85ccddb4e65fa58d1ce71146f8d530f0f1038
Unknown <geraldmoramora@gmail.com>
2018-05-27 22:51:16 -0600

    Todo lo hecho hasta el 27 de Mayo

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: String
+  name: String,
+  email: String,
+  pass: String
 });
 const User = mongoose.model('users', UserSchema);
 export default User;
--- src/schema.js
+++ src/schema.js
@@ -2,11 +2,13 @@ export default `
   type User {
     _id: String!
     name: String!
+    email: String
+    pass: String
   }
   type Query {
     allUsers: [User!]!
   }
   type Mutation {
-    createUser(name: String!): User!
+    createUser(name: String!, email: String, pass:String): User!
   }
 `;
